b1bf263bb54ac2958906da53e843bf0919d7292d
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-12-15 00:18:42 +0530

    fuzzer: improve prompt. generate multiple codes at once. change fuzzing parameters.

--- fuzz4py/fuzzer.py
+++ fuzz4py/fuzzer.py
@@ -3,6 +3,7 @@ import random
 import argparse
 import tqdm
 import time
+import json
 from fuzz4py.larker import random_grammar_excerpt
 import google.generativeai as genai
 from google.ai.generativelanguage_v1beta.types import content
@@ -20,12 +21,12 @@ script_path = os.path.dirname(os.path.realpath(__file__))
 
 class Fuzzer:
     operators = [
-        "GEN", # generate a new input
+        "GEN", "GEN", # generate a new input (x3 probability)
         "EQU", # generate semantically equivalent input to the last input
         "MUT", # mutate the last input
         "COM", # combine the last two inputs
     ]
-    LOOKBEHIND = 5 # number of previous inputs to look behind
+    LOOKBEHIND = 25 # number of previous inputs to look behind
 
     def __init__(self, system_prompt: str, inputs_directory: str = os.path.join(script_path, "inputs"), budget: int = 10):
         self.generation_config = {
@@ -33,7 +34,18 @@ class Fuzzer:
             "top_p": 0.95,
             "top_k": 40,
             "max_output_tokens": 8192,
-            "response_mime_type": "text/plain",
+            "response_schema": content.Schema(
+                type = content.Type.OBJECT,
+                properties = {
+                "codes": content.Schema(
+                    type = content.Type.ARRAY,
+                    items = content.Schema(
+                    type = content.Type.STRING,
+                    ),
+                ),
+                },
+            ),
+            "response_mime_type": "application/json",
         }
         self.model_params = {
             "model_name": "gemini-1.5-flash-8b",
@@ -51,8 +63,7 @@ class Fuzzer:
         # update the count by taking the maximum of the file names
         # and also update the last input
         if os.path.exists(self.inputs_directory):
-            inputs_dir_files = os.listdir(self.inputs_directory)
-            if "log.txt" in inputs_dir_files: inputs_dir_files.remove("log.txt") # remove the log file from the list
+            inputs_dir_files = [file for file in os.listdir(self.inputs_directory) if file.endswith(".py")]
             if inputs_dir_files:
                 max_file = max(map(lambda x: int(x.split(".")[0]), inputs_dir_files))
                 self.count = max_file + 1
@@ -74,20 +85,25 @@ class Fuzzer:
         if not self.previous_inputs:
             prompt = self.__generate_new_input()
             operator = "GEN"
+            signature = ""
         else:
             # choose an operator
             while True:
                 operator = random.choice(Fuzzer.operators)
+                signature = ""
                 try:
                     match operator:
                         case "GEN":
                             prompt = self.__generate_new_input()
                         case "EQU":
-                            prompt = self.__generate_equivalent_input()
+                            prompt, parent = self.__generate_equivalent_input()
+                            signature = str(parent)
                         case "MUT":
-                            prompt = self.__mutate_input()
+                            prompt, parent = self.__mutate_input()
+                            signature = str(parent)
                         case "COM":
-                            prompt = self.__combine_inputs()
+                            prompt, parent1, parent2 = self.__combine_inputs()
+                            signature = f"({parent1}, {parent2})"
                     break
                 except ValueError:
                     continue
@@ -96,26 +112,29 @@ class Fuzzer:
 
         try:
             chat_session = self.model.start_chat(history=[])
-            response = chat_session.send_message(prompt)
-            completion = response.text.split("```python", 1)[1].split("```")[0]
+            response = json.loads(chat_session.send_message(prompt).text)
+            
+            for completion in response["codes"][:5]: # don't use more than 5 completions 
+                completion = completion.split("```python", 1)[-1].split("```")[0]
 
-            # save the input
-            input_path = os.path.join(self.inputs_directory, f"{self.count}.py")
-            with open(input_path, "w") as f:
-                f.write(completion)
+                # save the input
+                input_path = os.path.join(self.inputs_directory, f"{self.count}.py")
+                with open(input_path, "w") as f:
+                    f.write(completion)
 
-            # log the generation of the input
-            with open(os.path.join(self.inputs_directory, "log.txt"), "a") as f:
-                f.write(f"{self.count}: {operator}\n")
+                # log the generation of the input
+                with open(os.path.join(self.inputs_directory, "log.txt"), "a") as f:
+                    f.write(f"{self.count}: {operator} {signature}\n")
 
-            # update the previous inputs list
-            self.previous_inputs.append((self.count, completion))
+                # update the previous inputs list
+                self.previous_inputs.append((self.count, completion))
 
-            # increment the count
-            self.count += 1
+                # increment the count
+                self.count += 1
         except GoogleAPICallError as e:
+            print(e)
             # check if the error is due to ResourceExhausted or QuotaExceeded
-            if "429" in str(e) and "ResourceExhausted" in str(e):
+            if "429" in str(e) and "Resource" in str(e):
                 global api_key_idx
                 print(f"API key {api_keys[api_key_idx]} has been exhausted. Switching to the next API key.")
                 api_key_idx += 1
@@ -127,27 +146,39 @@ class Fuzzer:
                 time.sleep(2) # wait for 2 seconds and try again
 
             return None
+        except Exception as e:
+            print(e)
+            return None
 
     def __generate_new_input(self):
         grammar_excerpt = random_grammar_excerpt(5)
         return "Generate a new python program. You can use the following excerpt from the python grammar.\n\n" + grammar_excerpt
 
     def __generate_equivalent_input(self):
-        seed = random.choice(self.previous_inputs[-Fuzzer.LOOKBEHIND:])[1]
-        return f"Generate another python program that is semantically similar to the following.\n```python\n{seed}\n```"
+        n = len(self.previous_inputs)
+        seed_index = random.choice(range(max(0, n - Fuzzer.LOOKBEHIND), n))
+        seed = self.previous_inputs[seed_index][1]
+
+        return f"Generate another python program that is semantically similar to the following.\n```python\n{seed}\n```", seed_index
 
     def __mutate_input(self):
-        seed = random.choice(self.previous_inputs[-Fuzzer.LOOKBEHIND:])[1]
-        return f"Mutate the following python program.\n```python\n{seed}\n```"
+        n = len(self.previous_inputs)
+        seed_index = random.choice(range(max(0, n - Fuzzer.LOOKBEHIND), n))
+        seed = self.previous_inputs[seed_index][1]
+
+        return f"Mutate the following python program.\n```python\n{seed}\n```", seed_index
 
     def __combine_inputs(self):
         if len(self.previous_inputs) < 2:
             raise ValueError("Not enough inputs to combine.")
 
-        seed1_item = random.choice(self.previous_inputs[-Fuzzer.LOOKBEHIND:])
-        seed2_item = random.choice(tuple(set(self.previous_inputs[-Fuzzer.LOOKBEHIND:]).difference({seed1_item})))
-        seed1, seed2 = seed1_item[1], seed2_item[1]
-        return f"Combine the following python programs.\n```python\n{seed1}\n```\n```python\n{seed2}\n```"
+        
+        n = len(self.previous_inputs)
+        seed1_index = random.choice(range(max(0, n - Fuzzer.LOOKBEHIND), n))
+        seed2_index = random.choice(tuple(set(range(max(0, n - Fuzzer.LOOKBEHIND), n)).difference({seed1_index}))) # choose without replacement
+        seed1, seed2 = self.previous_inputs[seed1_index][1], self.previous_inputs[seed2_index][1]
+
+        return f"Combine the following python programs.\n```python\n{seed1}\n```\n```python\n{seed2}\n```", seed1_index, seed2_index
 
     def fuzz(self):
         """
@@ -159,7 +190,7 @@ class Fuzzer:
             old_count = self.count
             self.__step()
             if self.count > old_count:
-                pbar.update(1)
+                pbar.update(self.count - old_count)
 
 
 if __name__ == "__main__":
@@ -176,7 +207,7 @@ if __name__ == "__main__":
         with open(args.prompt) as f:
             distilled_prompt = f.read()
 
-    system_prompt = "\n\n".join(["You are a fuzzer used to find bugs in python parsers. Please generate python programs which use language features in a complex way. These programs will not be executed and so focus on structures rather than actually working code. Do NOT run ast or exec on your own. You should use unnatural constructs and weird characters. Prefer very short programs."] + ([distilled_prompt] if distilled_prompt else []))
+    system_prompt = "\n\n".join(["You are a fuzzer used to find bugs in python parsers. Please generate python programs which use language features in a complex way. These programs will not be executed and so focus on structures rather than actually working code. Do NOT run ast or exec on your own. You should use unnatural constructs, complicated type annotations, weird characters, badly formatted comments, etc. Prefer very short programs and generate as many as you can."] + ([distilled_prompt] if distilled_prompt else []))
 
     fuzzer = Fuzzer(system_prompt, inputs_directory=args.inputs_directory, budget=args.budget)
     fuzzer.fuzz()
