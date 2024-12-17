e7d437f54b6b56243c11bfd6cf9d1ae30bfca035
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-12-17 11:22:12 +0530

    libfuzzing batch run file

---
+++ run_libfuzzing.sh
@@ -0,0 +1,4 @@
+python fuzz4py/libfuzzing/run_tests.py python3.11
+python fuzz4py/libfuzzing/run_tests.py python3.12
+python fuzz4py/libfuzzing/run_tests.py python3.13
+python fuzz4py/libfuzzing/run_tests.py graalpy
