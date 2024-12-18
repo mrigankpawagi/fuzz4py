658947c4b3c25119d1528b13d8f6e5f1bb87ff50
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-12-19 00:45:34 +0530

    put complete pipeline in run_libfuzzing. remove graalpy.

--- run_libfuzzing.sh
+++ run_libfuzzing.sh
@@ -1,4 +1,5 @@
-python fuzz4py/libfuzzing/run_tests.py python3.11
-python fuzz4py/libfuzzing/run_tests.py python3.12
+python fuzz4py/libfuzzing/input_generator.py
+python fuzz4py/libfuzzing/test_writer.py
 python fuzz4py/libfuzzing/run_tests.py python3.13
-python fuzz4py/libfuzzing/run_tests.py graalpy
+python fuzz4py/libfuzzing/run_tests.py python3.12
+python fuzz4py/libfuzzing/run_tests.py python3.11
